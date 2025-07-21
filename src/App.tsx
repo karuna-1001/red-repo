66f15f89617a11d00b9497f7461ff9371baa257e
Karunakaran Samayan <karunakaran.samayan@gmail.com>
2025-07-21 09:54:21 +0200

    add loader and error section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
-const App = ()=>{
-return(<h1>Repo List</h1>)}
+import { useState } from "react";
 
-export default App
+const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  return (
+      <div className="shadow-x1-20 mx-auto min-h-screen max-w-5xl p-6 text-center">
+        {!isLoading && (
+          <div className="animate-spin text-4xl leading-none text-red-700">
+            ⟳
+          </div>
+        )}
+        {!isError && (
+          <div className="mb-6 w-full animate-pulse bg-red-500 py-2 text-center text-red-50">
+            ⚠ Somthing went wrong. Please try again later.
+          </div>
+        )}
+    </div>
+    );
+};
+
+export default App;
